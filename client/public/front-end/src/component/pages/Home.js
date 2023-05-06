c82415b1d36559380217e783056d0e873b0e621e
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-05-07 02:28:44 +0530

    commited

--- client/public/front-end/src/component/pages/Home.js
+++ client/public/front-end/src/component/pages/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getQueryWeatherData } from '../utils/allApi';
+import { getQueryWeatherData } from '../utils/Api';
 import { Link } from 'react-router-dom';
 
 
