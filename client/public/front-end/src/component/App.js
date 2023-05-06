e46dcc06c8e1d9b25dc43c55d2ac8d200e574b49
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-05-07 02:28:19 +0530

    commented

--- client/public/front-end/src/component/App.js
+++ client/public/front-end/src/component/App.js
@@ -4,19 +4,26 @@ import CityDetail from './pages/CityDetail';
 
 function App() {
   return (
+    // Initialize the routing functionality using BrowserRouter
     <BrowserRouter>
+  {/* // Define the main body of the web page */}
       <body>
+    {/* // Define the main content area of the web page */}
         <main>
+      {/* // Define the routes for the web page using the Routes component */}
           <Routes>
 
+        {/* // Define the route for the Home page using the Home component */}
             <Route path="/" element={<Home />} />
+
+        {/* // Define the route for the CityDetail page using the CityDetail component */}
             <Route path='/:cityId' element={<CityDetail />} />
 
           </Routes>
         </main>
       </body>
-
     </BrowserRouter>
+
   );
 }
 
