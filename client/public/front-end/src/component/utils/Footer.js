a65e50a42afc2cf01b2dad68785af9b68ad88e4e
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-05-07 20:04:42 +0530

    added a footer

---
+++ client/public/front-end/src/component/utils/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/style.css'; // import the CSS file for styling
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                2023 Fidenz Technologies
+                
+                
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
