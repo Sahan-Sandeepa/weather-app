98b98bc69a51d45b5492e9baca8e2ea87a102c82
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-05-15 13:41:46 +0530

    changed the search component

--- client/public/front-end/src/component/common/SearchBar.js
+++ client/public/front-end/src/component/common/SearchBar.js
@@ -1,29 +1,21 @@
 import React, { useState } from 'react';
+import '../assets/style.css'
 
-function SearchBar(props) {
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
-    const handleSearch = (event) => {
-        event.preventDefault();
-    };
+function SearchBar() {
 
     return (
         <form className="flex justify-center items-center">
             <input
                 type="text"
                 placeholder="Enter a city"
-                onChange={handleInputChange}
+                onChange=""
                 className=" border-gray-300 rounded-l px-4 py-2 w-64 sm:w-80 md:w-96 focus:outline-none focus:ring-2 focus:ring-blue-500 input-box"
             />
 
             <button
                 type="submit"
-                onClick={handleSearch}
-                className="bg-purple-500 hover:bg-purple-800 rounded-r text-white font-semibold px-4 py-2"
+                onClick=""
+                className="hover:bg-purple-800 rounded-r text-white font-semibold px-4 py-2 search-bg"
             >
                 Add City
             </button>
